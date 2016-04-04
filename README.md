cceb9acdc39814acba7553d7c0106b2030e42845
Rafael <rafaelalves@cjr.org.br>
2016-04-03 21:48:32 -0300

    Gems e outras alterações

--- README.md
+++ README.md
@@ -3,13 +3,20 @@
 Esse é o projeto base de Rails da CJR. Ele deve ser clonado afim de iniciar um novo projeto. Ele possuí as seguintes caracteristicas:
 
 + Ruby Version: 2.2.3
-
 + Rails Version: 4.2.4
-
-+ Usa Slim
-
++ Usa Slim, Sass (SCSS)
 + Usa [Smacss](https://smacss.com/) para modularizar stylesheets
++ Usa Bootstrap
++ Usa FontAwesome
++ Usa Rails 12Factor
++ Usa Mailcatcher
++ Usa Rubocop para análise do código Ruby
 
-+ Usa Sass
+**ATENÇÃO**: crie um arquivo local_env.yml em config/ e coloque as seguintes informações:
+
+```
+GMAIL_USERNAME: 'email'
+GMAIL_PASSWORD: 'senha'
+```
 
 Para compilar esse arquivo, use `rake doc:app`
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,23 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Use o host abaixo para usar o MailCatcher
+  config.action_mailer.default_url_options = { :host => 'localhost:1025' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+
+  ActionMailer::Base.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :authentication => 'plain',
+    :enable_starttls_auto => true,
+    :domain => 'gmail.com',
+    :user_name => ENV["GMAIL_USERNAME"],
+    :password => ENV["GMAIL_PASSWORD"]
+  }
+
 end
